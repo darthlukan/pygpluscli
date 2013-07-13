816b066db49b2c0475010795d51799cd21c1e7d0
Brian Tomlinson <darthlukan@gmail.com>
2013-07-13 16:05:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+PyGplusCli
+##########
+
+Author: Brian Tomlinson
+Contact: darthlukan at gmail dot com
+License: GPLv2
+
+A simple CLI client for reading the most recent G+
+posts from those in your circles.
+
+Requires Python-2.7.x, google-api-python-client, 
+Developer API Key from Google APIs console.
+
+TODO:
+    1. Finish the program (Doesn't pull most recent just yet, grabs everything)
+    2. Tests
+    3. Reconfigure config setup for one-shot.
+    4. Write out detailed setup instructions for users.
---
+++ main.py
@@ -0,0 +1,134 @@
+import sys
+import time
+import json
+import gflags
+import httplib2
+from pprint import pprint
+from datetime import datetime, timedelta
+from oauth2client.tools import run
+from oauth2client.file import Storage
+from apiclient.discovery import build
+from oauth2client.client import OAuth2WebServerFlow
+
+from ConfigParser import SafeConfigParser
+
+
+class Auth(object):
+    """
+    Google Authorization object.  Uses Web App OAuth2 flow
+    to seit and get authentication so that we only have to interact
+    on initial setup.  Saves tokens in calendar.dat.  Needed for
+    sending events to Google Calendar.
+
+    Important: Everything is hardcoded, so no args needed.
+    """
+
+    def __init__(self):
+        self.http = ''
+        self.service = ''
+        self.FLAGS = gflags.FLAGS
+        self.secrets = ''
+
+        config = SafeConfigParser()
+        config.read('keys.dat')
+        self.DEVKEY = config.get('CONFIG', 'devkey')
+
+        with open('client_secrets.json') as secrets:
+            self.secrets = json.load(secrets)
+
+        self.FLOW = OAuth2WebServerFlow(
+            client_id=self.secrets['installed']['client_id'],
+            client_secret=self.secrets['installed']['client_secret'],
+            scope="https://www.googleapis.com/auth/plus.login",
+            user_agent='Playground/v1'
+        )
+
+        self.storage = Storage('secrets.dat')
+        self.credentials = self.storage.get()
+
+    def validate_creds(self):
+        """
+        Pulls credentials from storage, executes auth flow otherwise.
+
+        Returns credentials object.
+        """
+        if self.credentials is None or self.credentials.invalid is True:
+            self.credentials = run(self.FLOW, self.storage)
+        return self.credentials
+
+    def get_authed_http(self):
+        """
+        Sets up and returns an HTTP object authorized
+        with our credentials.
+        """
+        self.http = httplib2.Http()
+        self.http = self.credentials.authorize(self.http)
+        return self.http
+
+    def build_service(self):
+        """
+        Builds a service object based on authed http object,
+        developer API key, and API version arguments.
+
+        Returns service (object)
+        """
+        # Build a service object for interacting with the API. Visit
+        # the Google APIs Console to get a developerKey for your own application.
+        self.service = build(serviceName='plus', version='v1', http=self.http,
+                             developerKey=self.DEVKEY)
+        return self.service
+
+
+class App(object):
+
+    def __init__(self, service):
+        self.service = service
+        self.language = 'en'
+        self.myId = 'me'  # The authed user (from initial authflow in Auth class.
+        self.myCircledIds = []
+
+    def get_circled_people(self):
+        nextPageToken = None
+        while True:
+            request = self.service.people().list(userId=self.myId, collection='visible', pageToken=nextPageToken)
+            peopleResource = request.execute()
+            if 'items' in peopleResource:
+                for person in peopleResource['items']:
+                    self.myCircledIds.append(person['id'])
+                    print 'Got displayName %s with userid %s' % (person['displayName'], person['id'])
+            try:
+                nextPageToken = peopleResource['nextPageToken']
+            except KeyError:
+                break
+        return self.myCircledIds
+
+    def get_all_activities(self):
+        activitiesResource = self.service.activities()
+        for person in self.myCircledIds:
+            request = activitiesResource.list(userId=person['id'], collection='public', maxResults=2)
+            if request is not None:
+                activitiesDoc = request.execute()
+                if 'items' in activitiesDoc:
+                    for activity in activitiesDoc['items']:
+                        print activity['published'], activity['object']['content']
+                    print "======================================================="
+
+
+def main():
+    auth = Auth()
+    auth.validate_creds()
+    auth.get_authed_http()
+    service = auth.build_service()
+
+    print "We have a service object!"
+    pprint(service)
+
+    app = App(service)
+    people = app.get_circled_people()
+    pprint(people)
+
+    return sys.exit()
+
+
+if __name__ == '__main__':
+    main()
